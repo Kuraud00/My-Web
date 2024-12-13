295c3c2f2e3cd059d1d355e00f9f73cd2cc4c05e
Kuraud00 <2756345706@qq.com>
2024-12-13 18:34:00 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Kuraudo".toUpperCase());
+
+let username = "Kuraudo";
+
+if(username=="Kuraudo"){
+    console.log("YES");
+}else{
+    console.log("NO");
+}
+
+var obj = new Object();
+
+obj={
+    name:"Simon",
+    age:10,
+}
+
+console.log(obj);
